467f1a27f20c5ee23b8cf967724aeed3cb775e66
Pietro Marini <pmarini72107@gmail.com>
2025-01-23 13:39:17 +0100

    rebuilded code with customtkinter

--- app.py
+++ app.py
@@ -1,44 +1,55 @@
-import subprocess
-from apihandler import APIHandler
-from ydkhandler import YDKHandler
-from config import * # Import everything from config.py without having to name it every time
+try:
+    import os
+    import customtkinter as CTk
+    from PIL import Image
+    from apihandler import APIHandler
+    from ydkhandler import YDKHandler
+    from config import * # Import everything from config.py without having to name it every time
+except ImportError as e:
+    import os 
+    os.system("pip install -r requirements.txt") # Install the required packages
+    import customtkinter as CTk
+    from PIL import Image
+    from apihandler import APIHandler
+    from ydkhandler import YDKHandler
+    from config import * # Import everything from config.py without having to name it every time
 
-class App:
-    def __init__(self):
-        pg.init()
-        self.screen = pg.display.set_mode((800, 600))
-        pg.display.set_caption("Tracer")
-        self.clock = pg.time.Clock()
+# Initialize constants
+resolution_split: list[str] = WINDOW_RESOLUTION.split("x")
+WINDOW_WIDTH, WINDOW_HEIGHT = int(resolution_split[0]),int(resolution_split[1])
 
-        #Init utilities
-        self.ydk_handler = YDKHandler()
-        self.api_handler = APIHandler()
+# Initialize the APIHandler and YDKHandler classes
+api_handler = APIHandler()
+ydk_handler = YDKHandler()
 
-    def run(self):
-        while True:
-            self.handle_events()
-            self.update_logic()
-            self.render()
-            self.clock.tick(60)
+class App(CTk):
+    def __init__(self): # TODO put code below inside of this class
 
-    def handle_events(self):
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                self.quit()
 
-    def update_logic(self):
-        pass
+# Initialize the main window
+CTk.set_appearance_mode("dark") # Force appearance mode to dark
+root = CTk.CTk() # Create the main window class
+root.geometry(WINDOW_RESOLUTION) # Set the window resolution
+root.resizable(False, False) # Disable window resizing
+root.grid_columnconfigure(0, weight=1) # Set the column to expand with the window
+root.grid_rowconfigure(0, weight=1) # Set the row to expand with the window
 
-    def render(self):
-        self.screen.fill(BACKGROUND_COLOR)
+# Set centered window title and icon
+root.title("Tracer") # TODO center the title string
+if os.name == "nt": # If the OS is Windows
+    root.iconbitmap("./img/icon.ico")
 
-        pg.display.flip()
+# Initial menu window
 
-    def quit(self):
-        pg.quit()
-        exit()
+# Load the program logo
+program_logo = Image.open("./img/placeholder_icon.png") # Open the image with pillow
+program_logo_width, program_logo_height = program_logo.size # Get the size of the image
+program_logo_image = CTk.CTkImage(program_logo, size=(256,256)) # Load the program logo into ctk image class
+program_logo_label = CTk.CTkLabel(root, image=program_logo_image, text='') # Load the program logo into ctk label class
+program_logo_label.pack() # Pack the program logo label
+program_logo_label.place(x=WINDOW_WIDTH//2-program_logo_width//2, y=WINDOW_HEIGHT//2-program_logo_height//2-150) # Center the logo
 
-if __name__ == '__main__':
-    app = App()
-    #subprocess.run(["pip", "install", "-r", "requirements.txt"], check=True)
-    app.run()
+# Load new file button
+
+
+root.mainloop() # Start the main loop
--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 import pygame as pg
 
-BACKGROUND_COLOR = 'white'
 KNOWN_RESOLUTIONS = {
-    '1080p': (1920, 1080),
-    '720p': (1280, 720),
-    '480p': (640, 480),
-    '800x600': (800, 600),
-    '640x480': (640, 480)
+    '1080p': "1920x1080",
+    '720p': "1280x720",
+    '480p': "640x480",
+    '800x600': "800x600",
+    '640x480': "640x480"
 }
 WINDOW_RESOLUTION = KNOWN_RESOLUTIONS['720p']
